519c0880323e220c97d79d7e6386e437525a1f87
Jordan <www.jrdnbrj@hotmail.com>
2020-05-07 01:49:55 -0500

    Models Admin Created Successfully

--- apps/Homely/models.py
+++ apps/Homely/models.py
@@ -23,11 +23,11 @@ class Reseña(models.Model):
     id = models.AutoField(primary_key = True)
     usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
     fecha = models.DateTimeField()
-    models.TextField(max_length = 200, default = "")
+    texto = models.TextField(max_length = 200, default = "")
     calificacion = models.IntegerField()
 
     def __str__(self):
-        return "(" + str(self.id) + ") " + str(self.fecha) + " - " + self.usuario
+        return "(" + str(self.id) + ") " + str(self.fecha) + " - " + str(self.usuario)
 
 class Producto(models.Model):
     id = models.AutoField(primary_key = True)
@@ -37,10 +37,10 @@ class Producto(models.Model):
     descripcion = models.TextField(max_length = 200, default = "")
     numero_compras = models.IntegerField(default = 0, blank = True)
     precio = models.DecimalField(decimal_places = 4, max_digits = 7)
-    reseñas = models.ManyToManyField(Reseña)
+    reseñas = models.ManyToManyField(Reseña, blank = True)
 
     def __str__(self):
-        return "(" + str(self.id) + ") " + self.negocio
+        return "(" + str(self.id) + ") " + str(self.negocio)
 
 class Promocion(models.Model):
     id = models.AutoField(primary_key = True)
@@ -64,4 +64,4 @@ class Pedido(models.Model):
     estado = models.BooleanField(default = False)
 
     def __str__(self):
-        return "(" + str(self.id) + ") " + str(self.fecha) + " - " + self.usuario + " - " + str(self.valor)
+        return "(" + str(self.id) + ") " + str(self.fecha) + " - " + str(self.usuario) + " - " + str(self.valor)
